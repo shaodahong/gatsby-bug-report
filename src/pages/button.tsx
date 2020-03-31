d559f64a96f6fc56893f451ee0ac4db80641641b
shaodahong <darryshaw@gmail.com>
2020-03-31 15:53:40 +0800

    initial

---
+++ src/pages/button.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Button from "./index"
+
+export default function ButtonDemo() {
+  return <Button>CLick</Button>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Demo({ children }) {
+  return <button>{children}</button>
+}
